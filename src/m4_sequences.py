246e5260c3bca95da6e9ac88946d90c190a4ed4d
robertca <robertca@rose-hulman.edu>
2019-04-02 12:32:57 -0400

    DONE 4

--- src/m4_sequences.py
+++ src/m4_sequences.py
@@ -202,13 +202,18 @@ def practice_problem4b(sequence):
       :type sequence: (list | tuple) of (float | int)
     """
     # -------------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # DONE: 3. Implement and test this function.
     #     The testing code is already written for you (above).
     ###########################################################################
     # DIFFICULTY AND TIME RATINGS (see top of this file for explanation)
     #    DIFFICULTY:      5
     #    TIME ESTIMATE:   10 minutes.
     ###########################################################################
+    big = sequence[0]
+    for k in range(0, len(sequence), 2):
+        if sequence[k] > big:
+            big = sequence[k]
+    return big
 
 
 def run_test_practice_problem4c():
@@ -300,7 +305,7 @@ def practice_problem4c(points):
       :rtype: rg.Point | string
     """
     ###########################################################################
-    # TODO: 4. Implement and test this function.
+    # DONE: 4. Implement and test this function.
     #     The testing code is already written for you (above).
     #
     # IMPORTANT: This problem is your LOWEST PRIORITY for preparing
@@ -311,6 +316,15 @@ def practice_problem4c(points):
     #    DIFFICULTY:      9
     #    TIME ESTIMATE:   15 minutes.
     ###########################################################################
+    for k in range(len(points)):
+        if is_prime(points[k].x) is True:
+            if is_prime(points[k].y) is True:
+                y = points[k].y
+                x = points[k].x
+                points[k].x = y
+                points[k].y = x
+                return points[k]
+    return 'Not found'
 
 
 def run_test_practice_problem4d():
@@ -396,14 +410,20 @@ def practice_problem4d(sequence):
       :rtype: int
     """
     ###########################################################################
-    # TODO: 5. Implement and test this function.
+    # DONE: 5. Implement and test this function.
     #     The testing code is already written for you (above).
     ###########################################################################
     # DIFFICULTY AND TIME RATINGS (see top of this file for explanation)
     #    DIFFICULTY:      7
     #    TIME ESTIMATE:   15 minutes.
     ###########################################################################
-
+    total = 0
+    for k in range(len(sequence) - 1):
+        if sequence[k] != sequence[k + 1]:
+            if is_prime(sequence[k]) is True:
+                if is_prime(sequence[k + 1]) is True:
+                    total = total + sequence[k]
+    return total
 
 # -----------------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
